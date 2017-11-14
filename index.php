f313ab737113103986989ec38b5d9f9dc4f029e8
Claudio Rossi <claudio@eurosoftlab.com>
2017-11-14 09:08:21 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title>IFTS 2017</title>
+	</head>
+<body>
+	<p>
+		<?php
+			Hello();
+		?>
+	</p>	
+</body>
+</html>
+
+<?php
+
+/**
+ * Prints Hello World!
+ *
+ * @return void
+ */
+function Hello(){
+	echo "hello world!";
+}
+
+?>
